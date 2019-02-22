7e1cd51ac3683fb8e38616873d13a4859552dfdc
yunqian.yq <yunqian.yq@alibaba-inc.com>
2019-02-22 11:05:03 +0800

    asdas

---
+++ order_service/src/main/java/net/xdclass/order_service/service/ProductOrderService.java
@@ -0,0 +1,20 @@
+package net.xdclass.order_service.service;
+
+import net.xdclass.order_service.domain.ProductOrder;
+
+/**
+ * 订单业务类
+ */
+public interface ProductOrderService {
+
+
+    /**
+     * 下单接口
+     * @param userId
+     * @param productId
+     * @return
+     */
+     ProductOrder save(int userId, int productId);
+
+
+}
